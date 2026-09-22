3c16fc71a0653e36ab14e6d14527cab3baae895f
shivlilaborgave11 <shivborgave1121@gmail.com>
2026-09-22 08:34:44 +0530

    navbar render

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { PostDashboard } from './shared/components/post-dashboard/post-dashboard';
 import { Home } from './shared/components/home/home';
-import { Navbar } from './shared/components/navbar/navbar';
+
 
 export const routes: Routes = [
     {
         path: '',
-        component: Navbar
+        component: Home
     },
+
     {
         path: 'home',
         component: Home
